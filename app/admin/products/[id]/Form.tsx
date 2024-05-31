0f4fbf061f1cc334dceac66c64b425a834d11390
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-05-31 11:17:46 +0200

    Upload product images

--- app/admin/products/[id]/Form.tsx
+++ app/admin/products/[id]/Form.tsx
@@ -11,7 +11,7 @@ import useSWR from 'swr';
 import useSWRMutation from 'swr/mutation';
 
 export default function ProductEditForm({ productId }: { productId: string }) {
-  const { data, error } = useSWR(`/api/admin/products/${productId}`);
+  const { data: product, error } = useSWR(`/api/admin/products/${productId}`);
   const router = useRouter();
 
   const { trigger: updateProduct, isMutating: isUpdating } = useSWRMutation(
@@ -41,16 +41,16 @@ export default function ProductEditForm({ productId }: { productId: string }) {
   } = useForm<Product>();
 
   useEffect(() => {
-    if (!data) return;
-    setValue('name', data.name);
-    setValue('slug', data.slug);
-    setValue('price', data.price);
-    setValue('description', data.description);
-    setValue('image', data.image);
-    setValue('category', data.category);
-    setValue('brand', data.brand);
-    setValue('countInStock', data.countInStock);
-  }, [data, setValue]);
+    if (!product) return;
+    setValue('name', product.name);
+    setValue('slug', product.slug);
+    setValue('price', product.price);
+    setValue('description', product.description);
+    setValue('image', product.image);
+    setValue('category', product.category);
+    setValue('brand', product.brand);
+    setValue('countInStock', product.countInStock);
+  }, [product, setValue]);
 
   const formSubmit = async (formData: any) => {
     await updateProduct(formData);
@@ -59,7 +59,7 @@ export default function ProductEditForm({ productId }: { productId: string }) {
   if (error) {
     return <div>{error.message}</div>;
   }
-  if (!data) {
+  if (!product) {
     return <div>Loading...</div>;
   }
 
@@ -95,6 +95,34 @@ export default function ProductEditForm({ productId }: { productId: string }) {
     </div>
   );
 
+  const uploadImage = async (e: any) => {
+    const toastId = toast.loading('Uploading image...');
+    try {
+      const resSign = await fetch('/api/cloudinary-sign', {
+        method: 'POST',
+      });
+      const { signature, timestamp } = await resSign.json();
+      const file = e.target.files[0];
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('signature', signature);
+      formData.append('timestamp', timestamp);
+      formData.append('api_key', process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY!);
+      const res = await fetch(
+        `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+        {
+          method: 'POST',
+          body: formData,
+        }
+      );
+      const data = await res.json();
+      setValue('image', data.secure_url);
+      toast.success('Image uploaded successfully', { id: toastId });
+    } catch (err: any) {
+      toast.error(err.message, { id: toastId });
+    }
+  };
+
   return (
     <div>
       <h1 className='text-2xl py-4'>Edit Product {formatId(productId)}</h1>
@@ -102,11 +130,24 @@ export default function ProductEditForm({ productId }: { productId: string }) {
         <form onSubmit={handleSubmit(formSubmit)}>
           <FormInput name='Name' id='name' required />
           <FormInput name='Slug' id='slug' required />
-          <FormInput name='Price' id='price' required />
-          <FormInput name='Description' id='description' required />
           <FormInput name='Image' id='image' required />
+          <div className='md:flex mb-6'>
+            <label htmlFor='imageFile' className='label md:w-1/5'>
+              Upload image
+            </label>
+            <div className='md:w-4/5'>
+              <input
+                type='file'
+                className='file-input w-full max-w-md'
+                id='imageFile'
+                onChange={uploadImage}
+              />
+            </div>
+          </div>
           <FormInput name='Category' id='category' required />
+          <FormInput name='Description' id='description' required />
           <FormInput name='Brand' id='brand' required />
+          <FormInput name='Price' id='price' required />
           <FormInput name='Count In Stock' id='countInStock' required />
 
           <button
